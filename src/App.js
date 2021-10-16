a940e50c4b0c280cdf03bf4eacba12eeb56e31b9
Teerapong <teera04164@gmail.com>
2021-10-16 09:11:01 +0700

    starter project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span class="material-icons-outlined">
+          account_circle
+        </span>
         <a
           className="App-link"
           href="https://reactjs.org"
